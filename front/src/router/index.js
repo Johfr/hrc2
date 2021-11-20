0088a07514e3a9d7861ddbb8cc23e7b94e4c0f19
johanne.assorin <johanne.assorin@arneogroup.com>
2021-11-20 20:26:46 +0100

    hrc2 007: (fix) - fix du router (to, from, next) redirection teampage

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -25,10 +25,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "teamPage" */ '../views/TeamPage.vue'),
-    beforeEnter: (next) => {
+    beforeEnter: (to, from, next) => {
       const userId = localStorage.getItem('uuid')
       if (userId != 'null') {
         next()
+        // next({path: to.fullPath})
       } else {
         next('/')
       }
